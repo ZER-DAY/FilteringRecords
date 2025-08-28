6f53059d16e098014b622aae69f9b179202135a9
ZER-DAY <bahaa.ali.m.alhabil@gmail.com>
2025-08-28 19:50:26 +0300

    up

--- DataCheckResult.h
+++ DataCheckResult.h
@@ -1,1 +1,8 @@
 #pragma once
+#include <string>
+
+// Result of input validation.
+struct DataCheckResult {
+    bool isCorrect = true;   // overall validity flag
+    std::string reason;      // reason if invalid
+};
--- Record.h
+++ Record.h
@@ -1,1 +1,10 @@
 #pragma once
+#include <string>
+#include <map>
+#include "Property.h"
+
+// Record: unique name + dictionary of properties by name.
+struct Record {
+    std::string name;
+    std::map<std::string, Property> properties;
+};
