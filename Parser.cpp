5b4566b13b5bde3d50fd942a1f5e3ef8d590eaf8
ZER-DAY <bahaa.ali.m.alhabil@gmail.com>
2025-08-30 11:16:08 +0300

    parser: add class rule line parsing skeleton (phrases split; defer rule phrase parsing)

---
+++ Parser.cpp
@@ -0,0 +1,14 @@
+#include "Parser.h"
+
+// NOTE: Implementation will be added incrementally.
+// Keeping a minimal stub so the project keeps compiling.
+namespace Parser {
+    bool parseFile(const std::string&,
+        std::vector<Record>&,
+        std::vector<ClassRule>&,
+        std::string& errMsg)
+    {
+        errMsg = "parser not implemented yet";
+        return false;
+    }
+}
